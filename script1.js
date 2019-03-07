ffb81464b2c13029f895ee00502cba4b4ae7c645
Jack Elliott <jack.elliott02@bbc.co.uk>
2019-03-07 11:29:46 +0000

    updating script

--- script1.js
+++ script1.js
@@ -1,2 +1,14 @@
 console.log("Are you ready");
 console.log("I'm ready");
+
+var request = new XMLHttpRequest();
+
+var url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=getmimo";
+
+console.log(url);
+
+
+request.open("GET", url, false);
+request.send();
+
+console.log(request.response);
